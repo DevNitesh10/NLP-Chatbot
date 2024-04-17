96faddc70799a619c839ded4ce69e48889c3bd70
DevNitesh10 <princenitesh325@gmail.com>
2024-04-17 20:39:38 +0545

    Bugs Fixed

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi import Request
 from fastapi.responses import JSONResponse
 import db_helper
-# import generic_helper
+import generic_helper
 
 
 app = FastAPI()
